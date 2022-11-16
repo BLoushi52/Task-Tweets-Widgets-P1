529525f8c8dbdf3c01edc37728197c388d501cbb
BLoushi52 <k.albloushii@hotmail.com>
2022-11-16 17:04:26 +0300

    removed unnecessary assets

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -8,7 +8,7 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Image.asset(
-          "assets/twitter2.png",
+          "assets/twitter.png",
           height: 35,
         ),
       ),
